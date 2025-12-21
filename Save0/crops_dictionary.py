878d6b359b5687538d441f1045c402b06f3f6624
local-user <local@localhost>
2025-12-21 10:21:44 -0500

    Added a "crop dictionary" for later implementation with automated planting decision making

---
+++ Save0/crops_dictionary.py
@@ -0,0 +1,8 @@
+from __builtins__ import *
+# To be added: dictionary to translate from the "items" in the inventory to the "entities" to be planted
+crops_dictionary = {
+    Items.Carrot: Entities.Carrot,
+    Items.Pumpkin: Entities.Pumpkin,
+    Items.Wood: Entities.Tree,
+    Items.Hay: Entities.Grass,
+}
