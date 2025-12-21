caf44cda3c77975f9a984c063e7f6f43dfdee40c
local-user <local@localhost>
2025-12-21 08:57:31 -0500

    added a file: Save0/grass_harvest.py, special purpose script to harvest grass

---
+++ Save0/grass_harvest.py
@@ -0,0 +1,8 @@
+# Prepare a farm of just grass
+from __builtins__ import *
+clear()
+from field_lib import prepare_field, farm_pass
+size= get_world_size()
+prepare_field(size, Entities.Grass)
+while True:
+    farm_pass(size, Entities.Grass)
